b93d2070e424f7643d6687f03ceab0b48b8b5a3c
Panagiotis Pipis <pan.pipis@gmail.com>
2024-10-31 23:41:34 +0200

    fix message count in WS session

--- internal/pkg/httpsrv/stats.go
+++ internal/pkg/httpsrv/stats.go
@@ -15,14 +15,4 @@ func (w *sessionStats) inc() {
 	w.sent++
 }
 
-func (s *Server) incStats(id string) {
-	// Find and increment.
-	for _, ws := range s.sessionStats {
-		if ws.id == id {
-			ws.inc()
-			return
-		}
-	}
-	// Not found, add new.
-	s.sessionStats = append(s.sessionStats, sessionStats{id: id, sent: 1})
-}
+
