b3d5f2c8ce4372714826d2d8a83875f881aa87aa
Panagiotis Pipis <pan.pipis@gmail.com>
2024-10-31 23:48:13 +0200

    string generator to produce hexstring

--- pkg/util/string.go
+++ pkg/util/string.go
@@ -31,3 +31,28 @@ func RandString(n int) string {
 
 	return string(b)
 }
+// RandHexString returns a random hex string of length n.
+func RandHexString(n int) string {
+	const letterBytes = "0123456789ABCDEF"
+	const (
+		letterIdxBits = 4                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, randx.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = randx.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
