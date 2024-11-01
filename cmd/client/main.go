624505d57dc441c100c5cff89921d9633225e3ad
PanagiotisPipis <pan.pipis@gmail.com>
2024-11-02 00:16:03 +0200

    command line client
    ws client with multuple connections support

---
+++ cmd/client/main.go
@@ -0,0 +1,46 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/url"
+	"sync"
+	"os"
+	"os/signal"
+
+	goclient "goapp/internal/app/client"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func main() {
+	var requested_connections int
+	flag.IntVar(&requested_connections, "c", 1, "number of connections to ws")
+	flag.Parse()
+	log.SetFlags(0)
+	log.Println("connections:", requested_connections)
+
+	shutdown := make(chan struct{})
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	go func() {
+		<-interrupt
+		log.Println("interrupt")
+		close(shutdown)
+	}()
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/goapp/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	var wg sync.WaitGroup
+	for i := 0; i < requested_connections; i++ {
+		wg.Add(1)
+		new_connection := goclient.New()
+		go new_connection.Connect(i, u, shutdown, &wg)
+	}
+	wg.Wait()
+}
---
+++ internal/app/client/client.go
@@ -0,0 +1,68 @@
+package goclient
+
+import (
+	"log"
+	"net/url"
+	"sync"
+	"github.com/gorilla/websocket"
+
+)
+
+type Connection struct {
+	id int
+	socket *websocket.Conn
+}
+
+func New() *Connection {
+	con := new(Connection)
+	return con
+}
+
+func (connection *Connection) Connect(id int, u url.URL, shutdown chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var err error
+
+	connection.socket, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer connection.socket.Close()
+
+	done := make(chan struct{})
+
+	connection.id = id
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := connection.socket.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("[conn #%d] recv: %s", connection.id, message)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-shutdown:
+			log.Printf("Shutting down connection #%d", connection.id)
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := connection.socket.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+
+			<-done
+			
+			return
+		}
+	}
+}
